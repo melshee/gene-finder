db8b3eb483a3eb01e7be6fcdf6298e59183ff26e
Melissa Sheehan <mxsheehan7@gmail.com>
2018-02-10 19:56:52 -0800

    finds all genes in a given DNA strand

--- newgenefinder.py
+++ newgenefinder.py
@@ -1,26 +1,63 @@
 from random import *
-
 from dictionaries import amino_acids
 from dictionaries import amino_abbr
 from dictionaries import corresponding_nucleotides
 
+#returns a list of indeces that substring (sub) is found at
+def find_start_codons(dna_strand, offset, sub="ATG"): 
+  listindex = []
+  i = dna_strand.find(sub, offset)
+  while i >= 0:
+    listindex.append(i)
+    i = dna_strand.find(sub, i + 1)
+  return listindex
+
+def clip_to_stop_codon(dna_strand):
+  stop_codon_list = {dna_strand.find("TAA"), dna_strand.find("TAG"), dna_strand.find("TGA")}
+  stop_codon_list.remove(-1)
+  # stop_codon_list.remove(-1)
+  first_stop_codon = min(stop_codon_list)
+  first_stop_codon = first_stop_codon + 3 #to keep the 3 bases in the stop codon
+  # print dna_strand[:first_stop_codon]
+  return dna_strand[:first_stop_codon]
+
+def read(input_dna, offset):
+  start_codons_list = find_start_codons(input_dna, offset)
+  # print start_codons_list
+  strands = [] #all strands from start codon to end of strand
+  genes = [] #all strands from start codon to closest stop codon
+  for start_codon_index in start_codons_list:
+    strands.append(input_dna[start_codon_index:])
+  print strands #list of all genes (each start with ATG)
+  for strand in strands:
+    gene = clip_to_stop_codon(strand)
+    genes.append(gene)
+  print genes
+  longest = max(genes, key=len)
+  print longest
+  print len(longest)
+  return longest
+
+
 
 def main():
-    #gene_arr = ["","","","","",""] #array of size 6 to hold all 6 possible genes  
-    #input strand =  TCAATGTAACGCGCTACCCGGAGCTCTGGGCCCAAATTTCATCCACT
+    print 'in main'
 
+    gene_arr = ["","","","","",""] #array of size 6 to hold all 6 possible genes
+    # print gene_arr  
+    input_dna_strand =  "ATGCCCCCCCATGCCCCCCTGACCCCCATGCCCCTGA"
+    # input_dna_strand =  "TCAATGTAACGCGCTACCCGGAGCTCTGGGCCCAAATTTCATCCACT"
     #get the gene strand from user: test, TCAATGTAACGCGCTACCCGGAGCTCTGGGCCCAAATTTCATCCACT
     #this ones better: TCAATGCGCGCTACCCGGTAAAGCTCTGGGCCCAAATTTCATCCACT
 
-
-    #assume: given strand is the template strand (coding strand)
+    #assummption 1: given strand is the template strand (coding strand)
       #read dna strand (read all 3 ORFs)
-          # gene_arr[0] = read(input_dna_strand)
-          # gene_arr[1] = read(input_dna_strand - first index)
-          # gene_arr[2] = read(input_dna_strand - second index)
-    #assume: given strand is the complementary strand (negative of the coding strand)
+    gene_arr[0] = read(input_dna_strand, 0)
+    gene_arr[1] = read(input_dna_strand, 1)
+    gene_arr[2] = read(input_dna_strand, 2)
+    #assumption 2: given strand is the complementary strand (negative of the coding strand)
       #negate(input_strand) #method to negate dna strand 
-      #read dna strand (read all 3 ORFs)
+        #read dna strand (read all 3 ORFs)
           # gene_arr[3] = read(input_dna_strand)
           # gene_arr[4] = read(input_dna_strand - first index)
           # gene_arr[5] = read(input_dna_strand - second index)
@@ -31,14 +68,13 @@ def main():
     #longest_amino_acid = toAminoAcid(longest_mrna_strand)
     #print(longest_amino_acid)
 
-
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # methods to implement:
 # read(input_dna_strand) #returns length of LONGEST gene for that ORF (there might be multiple gene encodings on the SAME strand on the SAME ORF)
 # toMRNA(longest_dna_strand) #converts input_dna_strand into an mRNA strand
 # toAminoAcid(longest_mrna_strand) #converts input_mrna_strand into an amino acid sequence (one letter sequence)
 
-
+main()
 
 
 
